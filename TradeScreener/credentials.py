fcfe3f1ffbde2c27c54d0ba3604c6d6088f4e1d8
Abhijith S <59734625+abheaks@users.noreply.github.com>
2025-01-01 20:10:46 +0530

    update credentials

--- TradeScreener/credentials.py
+++ TradeScreener/credentials.py
@@ -1,3 +1,8 @@
-client_id = "FYERS_CLIENT_ID"
-secret_key = "FYERS_SECRET"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("SECRET_KEY")
 redirect_uri = "https://trade.fyers.in/api-login/redirect-uri/index.html"
